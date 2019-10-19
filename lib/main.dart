1f9709defe852e9fdb72c48f378f129c72c44a39
Kshitij Gupta <kshitijgm@gmail.com>
2019-10-19 17:18:13 +0530

    dice: Remove image assets, draw elements instead
    - It's a lot more efficient to draw things using flutter
      components than looking up assetBundle over and over.
    - The approach could probably be improved, but it replicates
      older functionality and design and doesn't seems to show
      better results in the profiler.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,168 @@
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+void main() => runApp(AppRoot());
+
+class AppRoot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue.shade900,
+          appBar: null,
+          body: DicePage(),
         ),
-        body: DicePage(),
-      ),
-    ),
-  );
+      );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber;
+  int rightDiceNumber;
+
+  @override
+  void initState() {
+    leftDiceNumber = _randDice;
+    rightDiceNumber = _randDice;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Stack(
+        children: <Widget>[
+          AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text('Dice is nice'),
+            centerTitle: true,
+          ),
+          Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => setState(() => leftDiceNumber = _randDice),
+                    child: _imgButton(leftDiceNumber),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => setState(() => rightDiceNumber = _randDice),
+                    child: _imgButton(rightDiceNumber),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+
+  int get _randDice => Random().nextInt(6) + 1;
+
+  Widget _imgButton(int diceNo) => CustomDice(diceNo: diceNo);
+}
+
+class CustomDice extends StatelessWidget {
+  final int diceNo;
+  final List<List<bool>> _grid = [
+    [true, false, true],
+    [true, true, true],
+    [true, false, true],
+  ];
+
+  final Map _data = {
+    1: [
+      [1, 1]
+    ],
+    2: [
+      [2, 0],
+      [0, 2]
+    ],
+    3: [
+      [2, 0],
+      [0, 2],
+      [1, 1]
+    ],
+    4: [
+      [2, 0],
+      [0, 2],
+      [2, 2],
+      [0, 0]
+    ],
+    5: [
+      [2, 0],
+      [0, 2],
+      [2, 2],
+      [0, 0],
+      [1, 1]
+    ],
+    6: [
+      [2, 0],
+      [0, 2],
+      [2, 2],
+      [0, 0],
+      [1, 0],
+      [1, 2]
+    ],
+  };
+
+  CustomDice({@required this.diceNo}) : assert(diceNo >= 1 && diceNo <= 6);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Widget> rows = [];
+    for (int i = 0; i < _grid.length; i++) {
+      List<Widget> tmp = [];
+      for (int j = 0; j < _grid[i].length; j++) {
+        tmp.add(Opacity(
+          opacity: _grid[i][j] && opacity(i, j) ? 1 : 0,
+          child: _diceIndicator,
+        ));
+      }
+      rows.add(Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: tmp,
+      ));
+    }
+    Widget col = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: rows,
+    );
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: LayoutBuilder(
+        builder: (context, constraints) => Container(
+            width: constraints.maxWidth,
+            height: constraints.maxWidth,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              border: Border.all(color: Colors.white, width: 16.0),
+            ),
+            child: Center(child: col)),
+      ),
+    );
   }
+
+  bool opacity(int i, int j) {
+    bool ret = false;
+    for (List x in _data[diceNo]) {
+      if (ListEquality().equals(x, [i, j])) {
+        ret = true;
+        break;
+      }
+    }
+    return ret;
+  }
+
+  Widget get _diceIndicator => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        width: 20.0,
+        height: 20.0,
+      );
 }
